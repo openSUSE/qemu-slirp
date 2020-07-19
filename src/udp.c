7a4840a57ec7dbc37cca1ab96f058a9610b26950
Samuel Thibault <samuel.thibault@ens-lyon.org>
2020-07-19 04:45:05 +0200

    udp, udp6, icmp, icmp6: Enable forwarding errors on Linux
    Not all icmp errors are reported as errno errors. Linux however lets us
    get them through a message error queue.
    
    Signed-off-by: Samuel Thibault <samuel.thibault@ens-lyon.org>

--- src/udp.c
+++ src/udp.c
@@ -288,6 +288,23 @@ int udp_attach(struct socket *so, unsigned short af)
             so->s = -1;
             return -1;
         }
+
+#ifdef __linux__
+        {
+            int opt = 1;
+            switch (af) {
+            case AF_INET:
+                setsockopt(so->s, IPPROTO_IP, IP_RECVERR, &opt, sizeof(opt));
+                break;
+            case AF_INET6:
+                setsockopt(so->s, IPPROTO_IPV6, IPV6_RECVERR, &opt, sizeof(opt));
+                break;
+            default:
+                g_assert_not_reached();
+            }
+        }
+#endif
+
         so->so_expire = curtime + SO_EXPIRE;
         insque(so, &so->slirp->udb);
     }
