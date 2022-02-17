openSUSE/qemu-slirp

commit 60535013c3e22408f5f39e0fe2cba7bd762b66af

bootp: add support for UEFI HTTP boot
Current bootp implementation is only one step away from supporting UEFI HTTP
boot in QEMU. The only missing bit is that the UEFI specification [1] requires
a vendor class identifier option (num 60) set to "HTTPClient" string present
in the DHCP response.

OVMF [2] indeed ignores the DHCP response and considers UEFI HTTP boot as
failed, if this option is not present.

With this change one would be able to configure QEMU user networking like
below:

$ qemu ... -nic user,tftp=tftp-root,bootfile=http://10.0.2.2/ipxe.efi

and boot the VM using UEFI HTTP boot instead of TFTP.

[1]: https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf
[2]: https://github.com/tianocore/edk2/tree/5302bd81d9ba0c9e7f2371a81c438ec919ec8e1e/OvmfPkg

Signed-off-by: Ignat Korchagin <ignat@cloudflare.com>

src/bootp.c
@@ -34,6 +34,8 @@
 
 #define LEASE_TIME (24 * 3600)
 
+#define UEFI_HTTP_VENDOR_CLASS_ID "HTTPClient"
+
 static const uint8_t rfc1533_cookie[] = { RFC1533_COOKIE };
 
 #define DPRINTF(fmt, ...) DEBUG_CALL(fmt, ##__VA_ARGS__)
@@ -337,6 +339,27 @@ static void bootp_reply(Slirp *slirp,
                           "omitting domain-search option.");
             } else {
                 memcpy(q, slirp->vdnssearch, val);
+                q += val;
+            }
+        }
+
+        /* this allows to support UEFI HTTP boot: according to the UEFI
+           specification, DHCP server must send vendor class identifier option
+           set to "HTTPClient" string, when responding to DHCP requests as part
+           of the UEFI HTTP boot
+
+           we assume that, if the bootfile parameter was configured as an http
+           URL, the user intends to perform UEFI HTTP boot, so send this option
+           automatically */
+        if (g_str_has_prefix(slirp->bootp_filename, "http://")) {
+            val = strlen(UEFI_HTTP_VENDOR_CLASS_ID);
+            if (q + val + 2 >= end) {
+                g_warning("DHCP packet size exceeded, "
+                          "omitting vendor class id option.");
+            } else {
+                *q++ = RFC2132_VENDOR_CLASS_ID;
+                *q++ = val;
+                memcpy(q, UEFI_HTTP_VENDOR_CLASS_ID, val);
                 q += val;
             }
         }

src/bootp.h
@@ -71,6 +71,7 @@
 #define RFC2132_MAX_SIZE 57
 #define RFC2132_RENEWAL_TIME 58
 #define RFC2132_REBIND_TIME 59
+#define RFC2132_VENDOR_CLASS_ID 60
 #define RFC2132_TFTP_SERVER_NAME 66
 
 #define DHCPDISCOVER 1

commit 9acc7b0ebd74fcf75ff7f82ba9bbb9aa090d5beb

bootp: fix g_str_has_prefix warning/critical
Fixes: commit 60535013c3e ("bootp: add support for UEFI HTTP boot")
Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

src/bootp.c
@@ -351,7 +351,7 @@ static void bootp_reply(Slirp *slirp,
            we assume that, if the bootfile parameter was configured as an http
            URL, the user intends to perform UEFI HTTP boot, so send this option
            automatically */
-        if (g_str_has_prefix(slirp->bootp_filename, "http://")) {
+        if (slirp->bootp_filename && g_str_has_prefix(slirp->bootp_filename, "http://")) {
             val = strlen(UEFI_HTTP_VENDOR_CLASS_ID);
             if (q + val + 2 >= end) {
                 g_warning("DHCP packet size exceeded, "